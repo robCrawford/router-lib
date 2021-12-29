dbae5f056f47709f09dc8c8063011726fd5a39fc
Rob Crawford <robert.crawford2@sky.uk>
2021-12-29 15:15:51 +0000

    Example routes

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,44 +1,112 @@
 import React, { FC } from "react";
 import { useHistory } from "react-router-dom";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
+        <header>
+          Signed in as Eufy
+        </header>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/">Hotel Search</Link></li>
+            <li><Link to="/voucher">Add Voucher</Link></li>
           </ul>
         </nav>
         <Switch>
-          <Route path="/about"><About /></Route>
-          <Route path="/users"><Users /></Route>
-          <Route path="/"><Home /></Route>
+          <Route path="/occupancy"><Occupancy /></Route>
+          <Route path="/rooms"><Rooms /></Route>
+          <Route path="/success"><Success /></Route>
+          <Route path="/voucher/confirm"><VoucherConfirm /></Route>
+          <Route path="/voucher"><Voucher /></Route>
+          <Route path="/"><Search /></Route>
         </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
-function UsersLink() {
+const HotelCta: FC<{ id: string }> =({ id }) => {
   const history = useHistory();
-  const handleClick = () => history.push('/users');
-  return <button type="button" onClick={handleClick}> Go to users </button>
+  const handleClick = () => history.push('/rooms?id=' + id);
+  return <div className="hotel-result">{id}<button type="button" onClick={handleClick}> Choose rooms </button></div>
 }
 
-function Home() {
-  return <><h2>Home</h2><UsersLink /></>;
+const HotelRoom: FC<{ id: string }> =({ id }) => {
+  const history = useHistory();
+  const handleClick = () => history.push('/occupancy?id=' + id);
+  return <div className="hotel-result">{id}<button type="button" onClick={handleClick}> Select </button></div>
 }
 
-function About() {
-  return <><h2>About</h2><UsersLink /></>;
+const Occupancy: FC = () => {
+  const history = useHistory();
+  const handleClick = () => history.push('/success');
+  const options = [...Array(10).keys()].map(k => <option key={k}>{k}</option>);
+
+  return (
+    <>
+      <h2>Occupancy</h2>
+      <form>
+        <div>Adults:</div>
+        <select> {options} </select>
+        <div>Children:</div>
+        <select> {options} </select>
+      </form>
+      <button type="button" onClick={handleClick}> Select </button>
+    </>
+  );
 }
 
-function Users() {
-  return <><h2>Users</h2><UsersLink /></>;
+const Search: FC = () => {
+  return (
+    <>
+      <h2>Hotel Search</h2>
+      <HotelCta id="afe0678" />
+      <HotelCta id="pj3478" />
+      <HotelCta id="or38d8" />
+    </>
+  );
+}
+
+const Rooms: FC = () => {
+  return (
+    <>
+      <h2>Rooms</h2>
+      <HotelRoom id="678" />
+      <HotelRoom id="478" />
+      <HotelRoom id="8d8" />
+    </>
+  );
+}
+
+const Success: FC = () => {
+  return (
+    <h2>Your booking has been made!</h2>
+  );
+}
+
+const Voucher: FC = () => {
+  return (
+    <>
+      <h2>Add A Voucher</h2>
+      <textarea placeholder="Enter Code" cols={50} />
+      <button><Link to="/voucher/confirm">Submit</Link></button>
+    </>
+  );
+}
+
+const VoucherConfirm: FC = () => {
+  const history = useHistory();
+  const handleClick = () => history.push('/');
+
+  return (
+    <>
+      <h2>Voucher Added!</h2>
+      <button type="button" onClick={handleClick}>Continue</button>
+    </>
+  );
 }
 
 export default App;
